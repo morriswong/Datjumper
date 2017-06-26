12f72592840dcfc8c10fc7de53e665a1b300e572
morriswong <morriswong1@gmail.com>
2017-06-26 11:51:41 +0800

    “Added platfrom modifications”

--- accelerometerTest/gyro/game2.js
+++ accelerometerTest/gyro/game2.js
@@ -1,13 +1,10 @@
-var NUMBER_OF_PLATFORM = 20
-
 var Jumper = function() {};
 Jumper.Play = function() {};
 
 Jumper.Play.prototype = {
 
   preload: function() {
-    this.load.image( 'heroUp', 'frame_right.png' );
-    this.load.image( 'heroDown', 'frameFall.png' );
+    this.load.image( 'hero', 'basketball.png' );
     this.load.image( 'pixel', 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/836/pixel_1.png' );
   },
 
@@ -39,38 +36,14 @@ Jumper.Play.prototype = {
     // cursor controls
     this.cursor = this.input.keyboard.createCursorKeys();
 
-    if (window.DeviceOrientationEvent) {//
-        window.addEventListener("deviceorientation", function () {//gyro
-            // console.log(event.alpha + ' : ' + event.beta + ' : ' + event.gamma);
-            processGyro(event.alpha, event.beta, event.gamma);
-        }, true);
-    }
-
-    var velocity = this.hero.body.velocity;
-    var self = this
-
-    function processGyro(alpha,beta,gamma){
-        if (gamma > 0){
-            if (velocity.x >= 400){
-                velocity.x = velocity.x
-            } else if (gamma > 15) {
-                velocity.x += (gamma * 1.5);
-            } else {
-                velocity.x += (gamma);
-            }
-
-            self.hero.scale.setTo(0.2, 0.2);
-        }else if (gamma < 0) {
-            if (velocity.x <= -400){
-                velocity.x = velocity.x
-            }else if (gamma < -15) {
-                velocity.x += (gamma * 1.5);
-            } else {
-                velocity.x += (gamma);
-            }
-            self.hero.scale.setTo(-0.2, 0.2);
-        }
-    }
+    gyro.frequency = 10;
+    var self = this;
+    // start gyroscope detection/
+    gyro.startTracking(function(o) {
+      // updating player velocity
+         self.hero.body.velocity.x += o.gamma/15;
+        //  self.hero.body.velocity.y += o.beta/8;
+    });
   },
 
   update: function() {
@@ -84,20 +57,20 @@ Jumper.Play.prototype = {
     this.cameraYMin = Math.min( this.cameraYMin, this.hero.y - this.game.height + 500 );
     this.camera.y = this.cameraYMin;
 
-    // for each platform, find out which is the highest
+    // hero collisions and movement
+    this.physics.arcade.collide( this.hero, this.platforms );
+    this.heroMove();
+
+    // for each plat form, find out which is the highest
     // if one goes below the camera view, then create a new one at a distance from the highest one
     // these are pooled so they are very performant
     this.platforms.forEachAlive( function( elem ) {
       this.platformYMin = Math.min( this.platformYMin, elem.y );
       if( elem.y > this.camera.y + this.game.height ) {
         elem.kill();
-        this.platformsCreateOne(this.rnd.integerInRange(0,this.world.width - 70), this.rnd.integerInRange(this.platformYMin - 100, this.platformYMin - 200), 100 );
+        this.platformsCreateOne(this.rnd.integerInRange(0,this.world.width -70), this.rnd.integerInRange(this.platformYMin -100, this.platformYMin - 200), 100 );
       }
     }, this );
-
-    // hero collisions and movement
-    this.physics.arcade.collide( this.hero, this.platforms );
-    this.heroMove();
   },
 
   shutdown: function() {
@@ -114,14 +87,15 @@ Jumper.Play.prototype = {
     // platform basic setup
     this.platforms = this.add.group();
     this.platforms.enableBody = true;
-    this.platforms.createMultiple( NUMBER_OF_PLATFORM, 'pixel' );
+    this.platforms.createMultiple( 200, 'pixel' );
 
     // create the base platform, with buffer on either side so that the hero doesn't fall through
     this.platformsCreateOne( -16, this.world.height - 16, this.world.width + 16 );
     // create a batch of platforms that start to move up the level
-    for( var i = 0; i < (NUMBER_OF_PLATFORM - 1); i++ ) {
-      this.platformsCreateOne( this.rnd.integerInRange( 0, this.world.width - 50 ), this.world.height - 100 * i , 100 );
+    for( var i = 0; i <= 45; i++ ) {
+    this.platformsCreateOne( this.rnd.integerInRange( 0, this.world.width - 50 ), this.world.height - 100*i, 100);
     }
+
   },
 
   platformsCreateOne: function( x, y, width ) {
@@ -136,8 +110,7 @@ Jumper.Play.prototype = {
 
   heroCreate: function() {
     // basic hero setup
-    this.hero = game.add.sprite( this.world.centerX, this.world.height - 36, 'heroUp' );
-    this.hero.scale.setTo(-0.2, 0.2);
+    this.hero = game.add.sprite( this.world.centerX, this.world.height - 36, 'hero' );
     this.hero.anchor.set( 0.5 );
 
     // track where the hero started and how much the distance has changed from that point
@@ -147,35 +120,28 @@ Jumper.Play.prototype = {
     // hero collision setup
     // disable all collisions except for down
     this.physics.arcade.enable( this.hero );
-    this.hero.body.gravity.y = 1500;
-    this.hero.body.velocity.y = -1500;
+    this.hero.body.gravity.y = 500;
+    this.hero.body.velocity.y = -1000;
     this.hero.body.checkCollision.up = false;
     this.hero.body.checkCollision.left = false;
     this.hero.body.checkCollision.right = false;
   },
 
   heroMove: function() {
-    // handle the left and right movement of the hero
-    // if( this.cursor.left.isDown ) {
-    //   this.hero.body.velocity.x = -400;
-    // } else if( this.cursor.right.isDown ) {
-    //   this.hero.body.velocity.x = 400;
-    // } else {
-    //   this.hero.body.velocity.x = 0;
-    // }
+     //handle the left and right movement of the hero
+     if( this.cursor.left.isDown ) {
+      this.hero.body.velocity.x = -400;
+     } else if( this.cursor.right.isDown ) {
+       this.hero.body.velocity.x = 400;
+     } else {
+         this.hero.body.velocity.x = 0;
+    }
 
     // handle hero jumping && this.cursor.up.isDown
     if(this.hero.body.touching.down) {
       this.hero.body.velocity.y = -1000;
+      navigator.vibrate([500]);
     }
-
-    if(this.hero.body.velocity.y >= 0){
-        this.hero.loadTexture('heroDown')
-    } else {
-        this.hero.loadTexture('heroUp')
-    }
-
-    console.log(this.hero.body.velocity.y);
 
     // wrap world coordinated so that you can warp from left to right and right to left
     this.world.wrap( this.hero, this.hero.width / 2, false );

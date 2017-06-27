038c33666f3b99758e6b3dbdc957b864a261770c
MiguelSuay <miguel@suayhk.com>
2017-06-27 16:49:02 +0800

    Add files via upload
    Player experience improvement, health bar, sounds, ...

---
+++ playgame.js
@@ -0,0 +1,270 @@
+var NUMBER_OF_PLATFORM = 20
+var background
+
+var playgame = function(game){};
+
+playgame.prototype = {
+
+  // preload: function() {
+  //   this.load.image( 'heroUp', 'assets/frame_right.png' );
+  //   this.load.image( 'heroDown', 'assets/frameFall.png' );
+  //   this.load.image( 'pixel', 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/836/pixel_1.png' );
+  // },
+
+  create: function() {
+
+  	var barConfig = {width: 450,
+    height: 40,
+    x: 250,
+    y: 40,
+    bg: {
+      color: '#651828'
+    },
+    bar: {
+      color: '#FEFF03'
+    },
+    animationDuration: 200,
+    flipped: false,
+    isFixedToCamera: true};
+    this.myHealthBar = new HealthBar(this.game, barConfig);
+    this.world.bringToTop(this.myHealthBar);
+    this.sfx = {
+        coin: this.game.add.audio('sfxcoin'),
+    };
+        // background color
+    this.stage.backgroundColor = '#6bf';
+    background = game.add.tileSprite(0, 0, game.width, game.height, "background3");
+    this.world.sendToBack(background);
+
+    // scaling
+    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    this.scale.pageAlignHorizontally = true;
+    this.scale.pageAlignVertically = true;
+    this.scale.setScreenSize( true );
+
+    // physics
+    this.physics.startSystem( Phaser.Physics.ARCADE );
+
+    // camera and platform tracking vars
+    this.cameraYMin = 99999;
+    this.platformYMin = 99999;
+    this.coinYmin = 99999;
+
+    // create platforms
+    this.platformsCreate();
+    this.coinsCreate();
+
+    // create hero
+    this.heroCreate();
+
+    // cursor controls
+    this.cursor = this.input.keyboard.createCursorKeys();
+
+    var self = this
+
+    if (window.DeviceOrientationEvent) {//
+        window.addEventListener("deviceorientation", function () {//gyro
+            // console.log(event.beta);
+            processGyro(event.alpha, event.beta, event.gamma);
+        }, true);
+    }
+
+    var velocity = this.hero.body.velocity;
+
+    function processGyro(alpha,beta,gamma){
+        if (gamma > 0 && self.hero){
+            if (velocity.x >= 400){
+                velocity.x = velocity.x
+            }
+            else if (gamma > 10) {
+                velocity.x += (gamma * 1.5);}
+            else {
+                velocity.x += gamma;
+            }
+
+            self.hero.scale.setTo(0.2, 0.2);
+        }else if (gamma < 0 && self.hero) {
+            if (velocity.x <= -400){
+                velocity.x = velocity.x
+            }
+            else if (gamma < -10) {
+                velocity.x += (gamma * 1.5);}
+            else {
+                velocity.x += gamma;
+            }
+            self.hero.scale.setTo(-0.2, 0.2);
+        }
+    }
+  },
+
+  update: function() {
+    background.position.y = this.camera.y;
+    // this is where the main magic happens
+    // the y offset and the height of the world are adjusted
+    // to match the highest point the hero has reached
+    this.world.setBounds( 0, -this.hero.yChange, this.world.width, this.game.height + this.hero.yChange );
+
+    // the built in camera follow methods won't work for our needs
+    // this is a custom follow style that will not ever move down, it only moves up
+    this.cameraYMin = Math.min( this.cameraYMin, this.hero.y - this.game.height + 500 );
+    this.camera.y = this.cameraYMin;
+
+    // for each platform, find out which is the highest
+    // if one goes below the camera view, then create a new one at a distance from the highest one
+    // these are pooled so they are very performant
+    this.platforms.forEachAlive( function( elem ) {
+        this.platformYMin = Math.min( this.platformYMin, elem.y );
+        if( elem.y > this.camera.y + this.game.height ) {
+            elem.kill();
+            this.platformsCreateOne(this.rnd.integerInRange(0,this.world.width - 70), this.rnd.integerInRange(this.platformYMin - 100, this.platformYMin - 200), 100 );
+        }
+    }, this );
+
+    //creating coins
+   //  this.coins.forEachAlive( function( elem ) {
+   //      this.coinYMin = Math.min( this.coinYMin, elem.y );
+   //      if( elem.y > this.camera.y + this.game.height ) {
+   //          elem.kill();
+   //          this.coinsCreateOne(this.rnd.integerInRange(0,this.world.width -70), this.rnd.integerInRange(this.coinYMin - 100, this.coinYMin - 200), 1 );
+   //      }
+   // }, this );
+
+   //Coin killer
+   this.game.physics.arcade.overlap(this.hero, this.coins, this.onHeroVsCoin, null, this);
+
+    // hero collisions and movement
+    this.physics.arcade.collide( this.hero, this.platforms );
+    this.heroMove();
+
+  },
+
+  shutdown: function() {
+    // reset everything, or the world will be messed up
+    this.world.setBounds( 0, 0, this.game.width, this.game.height );
+    this.cursor = null;
+    this.hero.destroy();
+    this.hero = null;
+    this.platforms.destroy();
+    this.platforms = null;
+  },
+
+  coinsCreate: function() {
+    // coins basic setup
+    this.coins = this.add.group();
+    this.coins.enableBody = true;
+    this.coins.createMultiple( 1000, 'coin' );
+    // create a batch of coins
+    for( var i = 0; i <= 980; i++ ) {
+        this.coinsCreateOne( this.rnd.integerInRange( 0, this.world.width - 50 ), this.world.height - 100*i, 2);
+    }
+  },
+
+  coinsCreateOne: function( x, y, width ) {
+    // this is a helper function since writing all of this out can get verbose elsewhere
+    var coin = this.coins.getFirstDead();
+    coin.reset( x, y );
+    coin.scale.x = width;
+    coin.scale.y = 2;
+    coin.body.immovable = true;
+    this.game.physics.enable(coin);
+    coin.animations.add('rotate', [0, 1, 2, 1], 6, true); // 6fps, looped
+    coin.animations.play('rotate');
+    return coin;
+   },
+
+   onHeroVsCoin: function(hero, coin) {
+       this.sfx.coin.play();
+       coin.kill();
+    },
+
+  platformsCreate: function() {
+    // platform basic setup
+    this.platforms = this.add.group();
+    this.platforms.enableBody = true;
+    this.platforms.createMultiple( NUMBER_OF_PLATFORM, 'pixel' );
+
+    // create the base platform, with buffer on either side so that the hero doesn't fall through
+    this.platformsCreateOne( -16, this.world.height - 16, this.world.width + 16 );
+    // create a batch of platforms that start to move up the level
+    for( var i = 0; i < (NUMBER_OF_PLATFORM - 1); i++ ) {
+      this.platformsCreateOne( this.rnd.integerInRange( 0, this.world.width - 50 ), this.world.height - 100 * i , 100 );
+    }
+  },
+
+  platformsCreateOne: function( x, y, width ) {
+    // this is a helper function since writing all of this out can get verbose elsewhere
+    var platform = this.platforms.getFirstDead();
+    platform.reset( x, y );
+    platform.scale.x = width;
+    platform.scale.y = 16;
+    platform.body.immovable = true;
+
+    if (game.rnd.between(0, 1)!=0){
+        platform.type = "double";
+        platform.tint =  0xF6FA05;
+        platform.scale.y = 22;
+        platform.overlap = function(){
+            kill.platform;
+        };
+    } else {
+        platform.type = "normal";
+    }
+
+    return platform;
+  },
+
+  heroCreate: function() {
+    // basic hero setup
+    this.hero = game.add.sprite( this.world.centerX, this.world.height - 36, 'heroUp' );
+    this.hero.scale.setTo(-0.2, 0.2);
+    this.hero.anchor.set( 0.5 );
+
+    // track where the hero started and how much the distance has changed from that point
+    this.hero.yOrig = this.hero.y;
+    this.hero.yChange = 0;
+
+    // hero collision setup
+    // disable all collisions except for down
+    this.physics.arcade.enable( this.hero );
+    this.hero.body.gravity.y = 1500;
+    this.hero.body.velocity.y = -1500;
+    this.hero.body.checkCollision.up = false;
+    this.hero.body.checkCollision.left = false;
+    this.hero.body.checkCollision.right = false;
+  },
+
+  heroMove: function() {
+    // handle the left and right movement of the hero
+    if( this.cursor.left.isDown ) {
+      this.hero.body.velocity.x = -400;
+    } else if( this.cursor.right.isDown ) {
+      this.hero.body.velocity.x = 400;
+    } else {
+      this.hero.body.velocity.x = 0;
+    }
+
+    // handle hero jumping && this.cursor.up.isDown
+    if(this.hero.body.touching.down) {
+      this.hero.body.velocity.y = -1000;
+    }
+
+    if(this.hero.body.velocity.y >= 0){
+        this.hero.loadTexture('heroDown')
+    } else {
+        this.hero.loadTexture('heroUp')
+    }
+
+    // console.log(this.hero.body.velocity.y);
+
+    // wrap world coordinated so that you can warp from left to right and right to left
+    this.world.wrap( this.hero, this.hero.width / 2, false );
+
+    // track the maximum amount that the hero has travelled
+    this.hero.yChange = Math.max( this.hero.yChange, Math.abs( this.hero.y - this.hero.yOrig ) );
+
+    // if the hero falls below the camera view, gameover
+    if( this.hero.y > this.cameraYMin + this.game.height && this.hero.alive ) {
+      this.state.start( 'GameOverScreen' );
+    }
+  }
+}
---
+++ preload.js
@@ -0,0 +1,28 @@
+var preload = function(game){};
+
+preload.prototype = {
+    preload: function(){
+		var loadingBar = this.add.sprite(game.width / 2, game.height / 2, "loading");
+		loadingBar.anchor.setTo(0.5,0.5);
+		game.load.setPreloadSprite(loadingBar);
+        game.load.image("background", "assets/sprites/background.png");  //preload background iamge for titlescreen
+        game.load.image("title", "assets/sprites/jump_opt.png");  //preload image for titlescreen
+        game.load.image("playbutton", "assets/sprites/playbutton2.png"); //preload image for titlescreen
+        game.load.image("background3", "assets/sprites/background3.png"); //preload background for playgame
+        game.load.image("background2", "assets/sprites/background2.png"); //preload background for game over
+        game.load.image("replay", "assets/sprites/replay.png"); //preload replay button
+        game.load.image("separator", "assets/sprites/separator.png");
+        game.load.bitmapFont("font", "assets/fonts/font.png", "assets/fonts/font.fnt");
+
+        game.load.image( 'heroUp', 'assets/sprites/frame_right.png' );
+        game.load.image( 'heroDown', 'assets/sprites/frameFall.png' );
+        game.load.spritesheet('coin', 'assets/sprites/coin_spritesheet.png', 22, 22);
+        game.load.audio('sfxcoin', 'assets/audio/coin.wav');
+        game.load.image( 'pixel', 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/836/pixel_1.png' );
+    },
+
+	create: function(){
+		console.log("preload started");
+        game.state.start("TitleScreen");
+	}
+}
